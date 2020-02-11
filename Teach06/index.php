7b7a2a4171f1c51bf14698348e719b7bbb686743
Jbatty62 <login@johnbatty.me>
2020-02-11 10:34:25 -0700

    added add topic functionality

--- Teach06/index.php
+++ Teach06/index.php
@@ -77,7 +77,7 @@ catch (PDOException $ex)
                     $stmt = $db->prepare("INSERT INTO topics (name) VALUES (:name);");
                     $stmt->bindValue(':name', $newTopic, PDO::PARAM_STR);
                     $stmt->execute();
-
+                    echo '<p> Inserted </p>';
                     $topics->push($db->lastInsertId("topics_topics_id_seq"));
                     }
                 
